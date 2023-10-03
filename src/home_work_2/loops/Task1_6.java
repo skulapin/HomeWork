04040423db41004c08dacf33cd67ca37880ea0ed
Sergei Kulapin <serg.kulapin@gmail.com>
2023-10-03 18:05:53 +0300

    Корректировка задания 1.6

--- src/home_work_2/loops/Task1_6.java
+++ src/home_work_2/loops/Task1_6.java
@@ -14,16 +14,52 @@ public class Task1_6 {
             System.out.println();
             for (int j = 2; j <= 5; j++) {
                 number = i * j;
-                System.out.print(j + " x " + i + " = " + number + "    ");
+                System.out.print(returnFormula(i, j, number));
             }
+        }
+
+        System.out.println();
+
+        for (int i = 0; i < 53; i++) {
+            System.out.print("-");
         }
 
         for (int i = 1; i <= 10; i++) {
             System.out.println();
-            for (int j = 6; j <= 10; j++) {
+            for (int j = 6; j <= 9; j++) {
                 number = i * j;
-                System.out.print(j + " x " + i + " = " + number + "    ");
+                System.out.print(returnFormula(i, j, number));
             }
         }
+    }
+
+    /**
+     * Собирает строку с выражением умножения двух чисел
+     *
+     * @param i      Второй множитель
+     * @param j      Первый множитель
+     * @param number Результат умножения
+     * @return Переменная типа String с выражением перемножения двух чисел
+     */
+    public static String returnFormula(int i, int j, int number) {
+
+        String result = j + " x ";
+
+        if (i < 10) {
+            result += " " + i + " = ";
+        } else {
+            result += i + " = ";
+        }
+
+        if (number < 10) {
+            result += " " + number;
+        } else {
+            result += number;
+        }
+
+        if (j != 5 && j != 9) {
+            result += " | ";
+        }
+        return result;
     }
 }
